aaa7ed72765836ab61070dddf8a2f6018ba5a699
Nathan Bernstein <bernstna@oregonstate.edu>
2023-10-20 14:50:11 -0700

    Finished Project
    Truck model now rides on terrain and follows polylines

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import {
     TimeInterval,
     Cartesian3,
     PathGraphics,
-    GeoJsonDataSource
+    GeoJsonDataSource,
+    HeightReference,
+    VelocityOrientationProperty
 } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "../src/css/main.css"
@@ -62,19 +64,6 @@ viewer.dataSources.add(
 );
 viewer.zoomTo(dataSource);
 
-const timeStepInSeconds = 30;
-const totalSeconds = timeStepInSeconds * (dataSource.length - 1);
-const start = JulianDate.fromIso8601("2020-03-09T23:10:00Z");
-const stop = JulianDate.addSeconds(start, totalSeconds, new JulianDate());
-viewer.clock.startTime = start.clone();
-viewer.clock.stopTime = stop.clone();
-viewer.clock.currentTime = start.clone();
-viewer.timeline.zoomTo(start, stop);
-// Speed up the playback speed 50x.
-viewer.clock.multiplier = 50;
-// Start playing the scene.
-viewer.clock.shouldAnimate = true;
-
 // The SampledPositionedProperty stores the position and timestamp for each sample along the radar sample series.
 const positionProperty = new SampledPositionProperty();
 let dataS
@@ -88,6 +77,18 @@ await fetch('Shapes/data.geojson')
     console.error('Error fetching GeoJSON:', error);
   });
 console.log(dataS.features.length);
+const timeStepInSeconds = 30;
+const totalSeconds = timeStepInSeconds * (dataS.features.length - 1);
+const start = JulianDate.fromIso8601("2020-03-09T23:10:00Z");
+const stop = JulianDate.addSeconds(start, totalSeconds, new JulianDate());
+viewer.clock.startTime = start.clone();
+viewer.clock.stopTime = stop.clone();
+viewer.clock.currentTime = start.clone();
+viewer.timeline.zoomTo(start, stop);
+// Speed up the playback speed .5x.
+viewer.clock.multiplier = .01;
+// Start playing the scene.
+viewer.clock.shouldAnimate = true;
 for (let i = 0; i < dataS.features.length -1; i++) {
   const dataPoint = dataS.features[i];
 
@@ -108,43 +109,74 @@ for (let i = 0; i < dataS.features.length -1; i++) {
     viewer.entities.add({
       description: `Location: (${longitude}, ${latitude})`,
       position: position,
-      point: { pixelSize: 10, color: Color.BLUE },
-      clampToGround: true,
+      point: { pixelSize: 10, color: Color.BLUE, clampToGround: true,},
     });
   }
 
 
 }
+// Create a SampledPositionProperty for the green dot's path.
+const greenDotPositionProperty = new SampledPositionProperty();
 
-/*
-// Sample the GeoJSON data and update the point entity's position
- // Get the GeoJSON data source
-const entities = dataSource.entities.values;
-const timeInterval = 1.0; // Time interval between samples in seconds
+// Populate greenDotPositionProperty with samples from positionProperty.
+for (let i = 0; i < dataS.features.length - 1; i++) {
+  const dataPoint = dataS.features[i];
+  const time = JulianDate.addSeconds(start, i * timeStepInSeconds, new JulianDate());
+  const coordinates = dataPoint.geometry.coordinates[0];
 
-entities.forEach((entity, entityIndex) => {
-  if (entity.polygon) {
-    const coordinates = entity.polygon.hierarchy.getValue().positions;
+  if (coordinates) {
+    const longitude = coordinates[0][0];
+    const latitude = coordinates[0][1];
+    const position = Cartesian3.fromDegrees(longitude, latitude);
 
-    coordinates.forEach((position, i) => {
-      const cartographic = Cartographic.fromCartesian(position);
-      const longitude = Math.toDegrees(cartographic.longitude);
-      const latitude = Math.toDegrees(cartographic.latitude);
+    greenDotPositionProperty.addSample(time, position);
+  }
+}
 
-      // Calculate the time for the sample
-      const time = JulianDate.addSeconds(
-        viewer.clock.startTime,
-        entityIndex * timeInterval + i,
-        new JulianDate()
-      );
-      console.log(longitude)
-      console.log(latitude)
-      // Add the sample to the position property
-      pointEntity.position.addSample(
-        time,
-        Cartesian3.fromDegrees(longitude, latitude)
-      );
-    });
+// Create the green dot entity with the path defined by greenDotPositionProperty.
+const carEntity = viewer.entities.add({
+  availability: new TimeIntervalCollection([new TimeInterval({ start: start, stop: stop })]),
+  point: { pixelSize: 30, color: Color.GREEN, heightReference: HeightReference.CLAMP_TO_GROUND, clampToGround: true, },
+  show: false,
+});
+
+// Update the green dot's position based on the current simulation time.
+viewer.clock.onTick.addEventListener(function (clock) {
+  const currentTime = clock.currentTime;
+  const position = greenDotPositionProperty.getValue(currentTime);
+  if (position) {
+    carEntity.position = position; // Set the position property directly
   }
 });
-*/
+
+// Make the camera track the moving green dot entity.
+viewer.trackedEntity = carEntity;
+async function loadModel() {
+  // Load the 3D model from a local file (assuming it's in the same folder).
+  const truckUri = 'Shapes/truck.glb'; // Adjust the path as needed.
+
+  // Create an entity for the truck and set its position, model, and orientation.
+  const truckEntity = viewer.entities.add({
+    availability: new TimeIntervalCollection([new TimeInterval({ start: start, stop: stop })]),
+    position: positionProperty,
+    model: {
+      uri: truckUri,
+      minimumPixelSize: 30, // Adjust the minimum pixel size as needed.
+      heightReference: HeightReference.CLAMP_TO_GROUND, // Set height reference here
+      clampToGround: true, // Ensure the model clamps to the ground
+    },
+    orientation: new VelocityOrientationProperty(positionProperty),
+  });
+
+  // Update the truck model's position based on the current simulation time.
+  viewer.clock.onTick.addEventListener(function (clock) {
+    const currentTime = clock.currentTime;
+    const position = positionProperty.getValue(currentTime);
+    if (position) {
+      truckEntity.position = position; // Set the position property directly
+    }
+  });
+  viewer.trackedEntity = truckEntity;
+}
+
+loadModel();
